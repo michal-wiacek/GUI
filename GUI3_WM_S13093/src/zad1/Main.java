cf49a0d994ae5beab60fd9831649be135753a8cf
michal-wiacek <michal.wiacek.90@gmail.com>
2015-03-28 23:17:25 +0100

    zrobiona lista
    Change-Id: I8c7cad34bbdbbe01c053ecc32d1383495447ba4c
    Signed-off-by: michal-wiacek

--- GUI3_WM_S13093/src/zad1/Main.java
+++ GUI3_WM_S13093/src/zad1/Main.java
@@ -8,23 +8,44 @@ package zad1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
 public class Main {
 
-  public static void main(String[] args) {
+public static void main(String[] args) {
     String fname = System.getProperty("user.home") + "/tab.txt";            
     try {
+		if (fname == null)
+			throw new FileNotFoundException();
     	
     	Scanner sc = new Scanner(new File(fname));
-		int nextInt = sc.nextInt();
-		System.out.println(nextInt);
-		
-		
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+    	if (sc.equals(args) || sc.next().equals(args)){
+			throw new NoSuchElementException();
+    	}
+			int nextInt = sc.nextInt();
+			ArrayList<Integer> a = new ArrayList<Integer>();
+			//a.add(0, nextInt);
+			while(sc.hasNextInt()){
+			
+				a.add(0, nextInt);
+				a.add(sc.nextInt());
+			}
+		System.out.println(a);
+	/*	Integer [] nums = a.toArray(new Integer[0]);
+	    for(int i = 0; i < nums.length; i++){
+	       System.out.println(nums[i]);
+	    }
+		*/
+
+    }
+    
+     catch (FileNotFoundException|NoSuchElementException e) {
+		System.out.println("***");
+		//e.printStackTrace();
 	}  
+    
   }
 }
