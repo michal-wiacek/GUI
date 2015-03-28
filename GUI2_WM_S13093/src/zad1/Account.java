3af37c0b76cdf9ce9791834cfffaa042c406a583
michal-wiacek <michal.wiacek.90@gmail.com>
2015-03-28 18:11:08 +0100

    zad 2

---
+++ GUI2_WM_S13093/src/zad1/Account.java
@@ -0,0 +1,36 @@
+package zad1;
+
+public class Account extends Person {
+
+	public Account(String n) {
+		super(n);
+	}
+
+	static double balance;
+	
+	public static void setInterestRate(double d) {
+		balance = 0;
+		
+	}
+
+	public void deposit(int i) {
+		
+		
+	}
+
+	public void transfer(Account account, int i) {
+		
+		
+	}
+
+	public void withdraw(int i) {
+		
+		
+	}
+
+	public void addInterest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ GUI2_WM_S13093/src/zad2/Finder.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *  @author Wiącek Michał S13093
+ *
+ */
+
+package zad2;
+
+
+public class Finder {
+
+	public Finder(String fname) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getIfCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getStringCount(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}    
