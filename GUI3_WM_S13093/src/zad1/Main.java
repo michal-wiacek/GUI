7ba1569ba95f90de51eb6ed12a7158b8d0d4f753
michal-wiacek <michal.wiacek.90@gmail.com>
2015-03-29 11:21:43 +0200

    zrobione
    Signed-off-by: michal-wiacek <michal.wiacek.90@gmail.com>

--- GUI3_WM_S13093/src/zad1/Main.java
+++ GUI3_WM_S13093/src/zad1/Main.java
@@ -1,51 +1,71 @@
 /**
  *
- *  @author Wiącek Michał S13093
+ * @author Wiącek Michał S13093
  *
  */
-
 package zad1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
+import java.util.List;
 import java.util.Scanner;
 
-
 public class Main {
+	public static void main(String[] args) {
+		String fname = System.getProperty("user.home") + "/tab.txt";
 
-public static void main(String[] args) {
-    String fname = System.getProperty("user.home") + "/tab.txt";            
-    try {
-		if (fname == null)
-			throw new FileNotFoundException();
-    	
-    	Scanner sc = new Scanner(new File(fname));
-    	if (sc.equals(args) || sc.next().equals(args)){
-			throw new NoSuchElementException();
-    	}
+		try {
+
+			Scanner sc = new Scanner(new File(fname));
+
 			int nextInt = sc.nextInt();
-			ArrayList<Integer> a = new ArrayList<Integer>();
-			//a.add(0, nextInt);
-			while(sc.hasNextInt()){
-			
-				a.add(0, nextInt);
-				a.add(sc.nextInt());
-			}
-		System.out.println(a);
-	/*	Integer [] nums = a.toArray(new Integer[0]);
-	    for(int i = 0; i < nums.length; i++){
-	       System.out.println(nums[i]);
-	    }
-		*/
+			// System.out.println(nextInt);
 
-    }
-    
-     catch (FileNotFoundException|NoSuchElementException e) {
-		System.out.println("***");
-		//e.printStackTrace();
-	}  
-    
-  }
-}
+			List<Integer> a = new ArrayList<Integer>();
+			a.add(nextInt);
+
+			while (sc.hasNext()) {
+				a.add(sc.nextInt());
+
+			}
+			// System.out.println(a);
+			sc.close();
+
+			Integer[] nums = a.toArray(new Integer[a.size()]);
+			// String lista = Arrays.toString(nums);
+			// System.out.println("lista: " + lista);
+
+			int max = Integer.MIN_VALUE;
+			// int element;
+			for (int i = 0; i < nums.length; i++) {
+				// element = nums[i];
+
+				System.out.print(nums[i] + " ");
+
+				if (nums[i] > max) {
+					max = nums[i];
+
+				}
+
+			}
+			System.out.println("");
+			System.out.println(max);
+
+			List<Integer> listaIndexow = new ArrayList();
+			int index = 0;
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] == max) {
+					index = i;
+					listaIndexow.add(index);
+				}
+			}
+				Integer[] ind = listaIndexow.toArray(new Integer[listaIndexow.size()]);
+				for (int x=0; x<ind.length;x++){
+				System.out.print(ind[x] + " ");
+				}
+			
+
+		} catch (FileNotFoundException e) {
+			System.out.println("***");
+}}}
